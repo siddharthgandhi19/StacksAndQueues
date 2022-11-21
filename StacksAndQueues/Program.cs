1a4a24f4df2feb559a5acb390fa0602b78196759
siddharth gandhi <siddharthgandhi@outlook.com>
2022-11-21 13:53:47 +0530

    [Siddharth]Add:Create Stack

--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace StacksAndQueues
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Stacks stack = new Stacks();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome to Stack Problem");
+                Console.WriteLine(" 1. Push");
+                Console.WriteLine("Enter above mention option");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        stack.Push(56);
+                        stack.Push(30);
+                        stack.Push(70);
+                        stack.Display();
+                        break;
+                    case 2:
+                        flag = false;
+                        Console.WriteLine("Try Again");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ StacksAndQueues/Stacks.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    public class Stacks
+    {
+        private Node top;
+        public Stacks()
+        {
+            this.top = null;
+        }
+        public void Push(int value) // add
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
